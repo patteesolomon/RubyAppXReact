46c6c30e685c4b353b6b81d6c518f0b31c9f773e
patteesolomon <patteesolomon@gmail.com>
2023-02-13 04:22:00 -0600

    working index

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,9 @@
+#You only need require calls for two use cases:
+
+#To load files under the lib directory.
+# To load gem dependencies that have require: false in the Gemfile.
+#require "application_controller"
+class UserController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root "user#index"
+  get '/user', to: 'user#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
